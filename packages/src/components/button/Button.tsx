34747d7bc993b7aabf875b078ac3f6c1b34caca7
Juliana Ribeiro <juribeiro.ru@gmail.com>
2025-07-01 19:06:22 -0300

    feat: improve button component and adjust utilities classes after major changes

--- packages/src/components/button/Button.tsx
+++ packages/src/components/button/Button.tsx
@@ -4,22 +4,22 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "../../utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-lg default-button-animation font-semibold",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-lg font-semibold",
   {
     variants: {
       variant: {
         default: "bg-brand-primary text-white",
-        secondary: "bg-brand-secondary text-white hover:bg-secondary/80",
+        secondary: "bg-brand-secondary text-white",
         tertiary: "bg-accent text-brand-primary",
         outline:
-          "outline-button-animation border border-2 border-brand-primary text-brand-primary",
-        link: " p-0 hover:scale-none hover:shadow-none link-button-animation",
+          "button-outline-hover border border-2 border-brand-primary text-brand-primary",
+        link: " p-0 hover:scale-none hover:shadow-none button-link-hover",
       },
       size: {
         sm: "rounded-md px-2 py-1",
         md: "rounded-md px-4 py-2",
         lg: "rounded-xl px-10 py-4",
-        icon: "rounded-xl p-3",
+        icon: "rounded-xl py-2.5 px-2",
       },
     },
     defaultVariants: {
@@ -32,14 +32,22 @@ const buttonVariants = cva(
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
+    hoverEffect?: boolean
   }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, hoverEffect = true, variant, size, asChild = false, ...props },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          buttonVariants({ variant, size }),
+          hoverEffect ? "button-hover-effect" : "hover:brightness-115",
+          className
+        )}
         ref={ref}
         {...props}
       />
