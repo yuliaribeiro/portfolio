473be17e159bd2b1cb575512221a8bad5697aea8
Juliana Ribeiro <juribeiro.ru@gmail.com>
2025-07-05 14:10:11 -0300

    chore: update project to use i18n on contact section

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -1,14 +1,16 @@
 /* EN files */
-import homeEn from "./locales/en/homeEn.json"
 import aboutEn from "./locales/en/aboutEn.json"
+import contactEn from "./locales/en/contactEn.json"
+import homeEn from "./locales/en/homeEn.json"
 import skillsEn from "./locales/en/skillsEn.json"
 
 /* PT files */
 
 export const resources = {
   en: {
-    home: homeEn,
     about: aboutEn,
+    contact: contactEn,
+    home: homeEn,
     skills: skillsEn,
   },
 } as const
