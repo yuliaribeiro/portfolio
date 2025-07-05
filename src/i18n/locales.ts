409dc495fe2dfcb36234350edc6432db55b055f2
Juliana Ribeiro <juribeiro.ru@gmail.com>
2025-07-05 18:20:45 -0300

    feat: create the i18n files to support portuguese language

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -6,6 +6,11 @@ import projectsEn from "./locales/en/projectsEn.json"
 import skillsEn from "./locales/en/skillsEn.json"
 
 /* PT files */
+import aboutPt from "./locales/pt/aboutPt.json"
+import contactPt from "./locales/pt/contactPt.json"
+import homePt from "./locales/pt/homePt.json"
+import projectsPt from "./locales/pt/projectsPt.json"
+import skillsPt from "./locales/pt/skillsPt.json"
 
 export const resources = {
   en: {
@@ -14,5 +19,12 @@ export const resources = {
     home: homeEn,
     projects: projectsEn,
     skills: skillsEn,
+  },
+  pt: {
+    about: aboutPt,
+    contact: contactPt,
+    home: homePt,
+    projects: projectsPt,
+    skills: skillsPt,
   },
 } as const
