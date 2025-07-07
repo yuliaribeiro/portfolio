c9565d6a7f832667de85b19b60828ed1d4478c07
Juliana Ribeiro <juribeiro.ru@gmail.com>
2025-07-07 16:56:19 -0300

    feat: add path to render company logos on projects section

--- src/sections/projects/components/Projects.tsx
+++ src/sections/projects/components/Projects.tsx
@@ -8,6 +8,7 @@ type ProjectsInfo = {
   description: string
   stacks: string[]
   link: string
+  logo?: string
 }
 type ProjectsProps = {
   projectsInfo: ProjectsInfo[]
@@ -22,40 +23,46 @@ export const Projects = ({ pageTitle, projectsInfo }: ProjectsProps) => {
           {pageTitle}
         </h2>
         <div className="md: grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
-          {projectsInfo.map(({ title, description, stacks, link }, index) => (
-            <Card
-              className="flex flex-col justify-between gap-3 space-y-2"
-              key={`${index}-${title}`}
-            >
-              <div className="from-brand-primary/20 to-accent/10 -mx-4 -mt-4 h-48 bg-gradient-to-br"></div>
-              <h3 className="font-display text-foreground font-family-playfair text-2xl font-bold tracking-wide">
-                {title}
-              </h3>
-
-              <div className="space-y-2">
-                {/* Description */}
-                <p className="text-foreground-muted text-lg leading-relaxed">
-                  {description}
-                </p>
-                {/* Stacks */}
-                <div className="flex gap-3 pb-2">
-                  {stacks.map((stack, index) => (
-                    <Badge key={`${index}-${stack}`}>{stack}</Badge>
-                  ))}
+          {projectsInfo.map(
+            ({ title, description, stacks, link, logo }, index) => (
+              <Card
+                className="flex flex-col justify-between gap-3 space-y-2"
+                key={`${index}-${title}`}
+              >
+                <div className="from-brand-primary/50 to-accent/20 -mx-4 -mt-4 flex h-48 items-center bg-gradient-to-br">
+                  {logo && (
+                    <img className="mx-auto" src={logo} alt={`${title} logo`} />
+                  )}
                 </div>
-              </div>
+                <h3 className="font-display text-foreground font-family-playfair text-2xl font-bold tracking-wide">
+                  {title}
+                </h3>
 
-              {/* Links */}
-              <div>
-                <Button variant="link" asChild>
-                  <a href={link} target="_blank" rel="noopener noreferrer">
-                    <ExternalLink className="size-5" />
-                    Live
-                  </a>
-                </Button>
-              </div>
-            </Card>
-          ))}
+                <div className="space-y-2">
+                  {/* Description */}
+                  <p className="text-foreground-muted text-lg leading-relaxed">
+                    {description}
+                  </p>
+                  {/* Stacks */}
+                  <div className="flex gap-3 pb-2">
+                    {stacks.map((stack, index) => (
+                      <Badge key={`${index}-${stack}`}>{stack}</Badge>
+                    ))}
+                  </div>
+                </div>
+
+                {/* Links */}
+                <div>
+                  <Button variant="link" asChild>
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      <ExternalLink className="size-5" />
+                      Live
+                    </a>
+                  </Button>
+                </div>
+              </Card>
+            )
+          )}
         </div>
       </div>
     </section>
